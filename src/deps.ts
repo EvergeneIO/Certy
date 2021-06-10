2294c7d2379b372a8b019577b73107ce9f1736f7
NewtTheWolf <NewtTheWolf@users.noreply.github.com>
2021-06-10 13:35:54 +0000

    change: prettier code

--- src/deps.ts
+++ src/deps.ts
@@ -1,5 +1,5 @@
 export { config } from "https://deno.land/x/dotenv@v2.0.0/mod.ts";
-export * from "https://denopkg.com/discordeno/discordeno@main/mod.ts"
+export * from "https://denopkg.com/discordeno/discordeno@main/mod.ts";
 
 // einfach alle da rein packen
 
--- src/mod.ts
+++ src/mod.ts
@@ -4,23 +4,23 @@ import { config } from "./deps.ts";
 config({ export: true });
 
 const configs = {
-    DOMAINS: Deno.env.get('DOMAINS'),
-    HOST: Deno.env.get('DB_MYSQL_HOST'),
-    PORT: Deno.env.get('DB_MYSQL_PORT'),
-    USER: Deno.env.get('DB_MYSQL_USER'),
-    PASSWORD: Deno.env.get('DB_MYSQL_PASSWORD'),
-    NAME: Deno.env.get('DB_MYSQL_NAME'),
-    SQLITE: Deno.env.get('DB_SQLITE_FILE')
-}
+  DOMAINS: Deno.env.get("DOMAINS"),
+  HOST: Deno.env.get("DB_MYSQL_HOST"),
+  PORT: Deno.env.get("DB_MYSQL_PORT"),
+  USER: Deno.env.get("DB_MYSQL_USER"),
+  PASSWORD: Deno.env.get("DB_MYSQL_PASSWORD"),
+  NAME: Deno.env.get("DB_MYSQL_NAME"),
+  SQLITE: Deno.env.get("DB_SQLITE_FILE"),
+};
 
 if (!configs.SQLITE) {
-    const dirNames: string[] = [];
-    for await (const dirEntry of Deno.readDir('./certs')) {
-        if (dirEntry.isDirectory) {
-            dirNames.push(dirEntry.name);
-        }
+  const dirNames: string[] = [];
+  for await (const dirEntry of Deno.readDir("./certs")) {
+    if (dirEntry.isDirectory) {
+      dirNames.push(dirEntry.name);
     }
-    console.log(dirNames);
+  }
+  console.log(dirNames);
 } else {
-    console.log('SQLITE active')
+  console.log("SQLITE active");
 }
