0b3586255af4258feca55f37b65c3679cc1f9cbc
NewtTheWolf <59208097+NewtTheWolf@users.noreply.github.com>
2021-06-11 17:13:46 +0200

    for win users

--- src/mod.ts
+++ src/mod.ts
@@ -1,20 +1,9 @@
 import { copyCerts, configs, yamlParser, } from "./utils/mod.ts"
 import { DB, blue, gray } from "./deps.ts"
 import { log } from "./utils/logger.ts";
+import { sheduleTask } from "./utils/tasks.ts";
+import { con } from "./database/connection.ts";
 log.info('Certy Started');
-
-async function getNames(currentPath: string) {
-    const names: string[] = [];
-    for await (const dirEntry of Deno.readDir(currentPath)) {
-        const entryPath = `${currentPath}/${dirEntry.name}`;
-        names.push(entryPath);
-        if (dirEntry.isDirectory) {
-            names.concat(await getNames(entryPath));
-        }
-    }
-    return names;
-}
-console.log(await getNames('./config'));
 
 console.log(blue([
     gray(`\n**************************************************************\n`),
@@ -29,48 +18,48 @@ console.log(blue([
     `${gray("*")}                                                            ${gray("*")}\n`,
     gray(`**************************************************************\n`)].join("")
 ));
-
-setInterval(async function () {
+/* setInterval(async function () {
     console.log("10 sind um");
-}, 1000 * 10);
-
-const watcher = Deno.watchFs("./config");
-console.log("the watcher", watcher)
-for await (const watched of watcher) {
-    console.log(watched)
-}
-
-let watch
-if (Deno.env.get("ENV") == 'dev') {
-    watch = ["./letsencrypt/live", "./data/certy/config"]
+}, 1000 * 10); */
+if ('windows' == 'windows') {
+    log.info("Intervall Started!")
+    sheduleTask({
+        name: "Certy",
+        interval: 10000,
+        execute() {
+            main();
+        }
+    })
 } else {
-    watch = ["./letsencrypt/live", "./config"]
+    const watcher = Deno.watchFs(["./letsencrypt/live", "./config"]);
+    log.info("Watcher Started!")
+    for await (const event of watcher) {
+        main();
+    }
 }
 
-// const watcher = Deno.watchFs(watch);
-// for await (const event of watcher) {
-//     console.log(event)
-//     log.info('WATCHING OWO')
-//     const data = await yamlParser();
-//     if (!data) continue;
-//     if (!configs.SQLITE) {
-//         const database = await import("./database/connection.ts");
-//         /**
-//          * TODO: edge cases
-//          * mail.domain.photo
-//          * mail.domain.photography
-//          * co.uk
-//          */
-//         for (const [domain, location] of Object.entries(data)) {
-//             const res = (await database.con.query(`SELECT * FROM proxy_host WHERE (domain_names LIKE '%"${domain}"%') AND is_deleted = 0`))[0]
-//             console.log(blue('OWO WHATS THIS'))
-//             await copyCerts(domain, location, res)
-//         }
-//     } else {
-//         const db = new DB(configs.SQLITE);
-//         for (const [domain, location] of Object.entries(data)) {
-//             const res = [...db.query(`SELECT * FROM proxy_host WHERE (domain_names LIKE '%"${domain}"%') AND is_deleted = 0`).asObjects()][0];
-//             await copyCerts(domain, location, res)
-//         }
-//     }
-// }
+
+async function main() {
+    const data = await yamlParser();
+    if (!data) return;
+
+    if (!configs.SQLITE) {
+        /**
+         * TODO: edge cases
+         * mail.domain.photo
+         * mail.domain.photography
+         * co.uk
+         */
+        for (const [domain, location] of Object.entries(data)) {
+            const res = (await con.query(`SELECT * FROM proxy_host WHERE (domain_names LIKE '%"${domain}"%') AND is_deleted = 0`))[0]
+            console.log(blue('OWO WHATS THIS'))
+            await copyCerts(domain, location, res)
+        }
+    } else {
+        for (const [domain, location] of Object.entries(data)) {
+            const res = [...(con as DB).query(`SELECT * FROM proxy_host WHERE (domain_names LIKE '%"${domain}"%') AND is_deleted = 0`).asObjects()][0];
+            await copyCerts(domain, location, res)
+        }
+        con.close();
+    }
+}
